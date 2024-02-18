af6055c8932d56345f60dab5ae02d8410e854934
SimonWithoutGarfunkle <123770440+SimonWithoutGarfunkle@users.noreply.github.com>
2024-02-18 18:53:05 +0100

    initial commit

---
+++ src/main/java/fr/perso/morpion/MorpionApplication.java
@@ -0,0 +1,13 @@
+package fr.perso.morpion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorpionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorpionApplication.class, args);
+	}
+
+}
