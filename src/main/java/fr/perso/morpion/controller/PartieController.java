c7a53d57c9df668f017101c604e72d864462e958
SimonWithoutGarfunkle <simonlefort@hotmail.fr>
2024-02-27 15:21:22 +0100

    ajout de comments sur le controller

--- src/main/java/fr/perso/morpion/controller/PartieController.java
+++ src/main/java/fr/perso/morpion/controller/PartieController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
+/**
+ * Controller principal de l'application
+ * Gere les parties et les tours
+ */
 @RestController
 @RequestMapping("/api/partie")
 public class PartieController {
@@ -18,16 +22,33 @@ public class PartieController {
 
     private static final Logger logger = Logger.getLogger(LoggerFactory.class.getName());
 
+    /**
+     * Demande la grille de la partie en cours
+     *
+     * @param id de la partie
+     * @return un tableau de characteres qui represente la grille de jeu à jour
+     */
     @GetMapping("/{id}")
     public Character[] recupererGrille(@PathVariable(value = "id")Integer id) {
         return partieService.recupererGrille(partieService.getPartieById(id));
     }
 
+    /**
+     * Nouveau tour pour la partie en cours
+     *
+     * @param emplacement à jouer
+     * @param id de la partie
+     */
     @PostMapping("/{id}/tour")
     public void jouerTour(@RequestParam int emplacement, @PathVariable(value = "id") Integer id) {
         partieService.ajouterTourAPartie(id, emplacement);
     }
 
+    /**
+     * Demarre une nouvelle partie
+     *
+     * @return l'id de la nouvelle partie
+     */
     @GetMapping("/new")
     public int nouvellePartie() {
         return partieService.commencerPartie("Joueur 1", "Joueur 2");
